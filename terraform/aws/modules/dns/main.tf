a4677d7aea43fa540f98b605c180f8ee73037f99
LuizFelipeAlvesMaia <luiz.felipeam@hotmail.com>
2025-11-06 19:35:35 -0300

    feat: Add Grafana DNS records and outputs to the Cloudflare module

--- terraform/aws/modules/dns/main.tf
+++ terraform/aws/modules/dns/main.tf
@@ -18,3 +18,20 @@ resource "cloudflare_record" "argocd" {
     create_before_destroy = true
   }
 }
+
+# DNS Record para Grafana
+resource "cloudflare_record" "grafana" {
+  count = var.create_grafana_record ? 1 : 0
+
+  zone_id         = var.cloudflare_zone_id
+  name            = var.grafana_subdomain
+  type            = "CNAME"
+  content         = data.aws_lb.eks_nlb[0].dns_name
+  proxied         = var.proxied
+  ttl             = var.proxied ? 1 : var.ttl
+  allow_overwrite = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- terraform/aws/modules/dns/output.tf
+++ terraform/aws/modules/dns/output.tf
@@ -12,3 +12,18 @@ output "argocd_url" {
   description = "Full URL for ArgoCD access"
   value       = var.create_argocd_record ? "https://${var.argocd_subdomain}.${var.domain_name}" : null
 }
+
+output "grafana_dns_name" {
+  description = "DNS name for Grafana"
+  value       = var.create_grafana_record ? "${var.grafana_subdomain}.${var.domain_name}" : null
+}
+
+output "grafana_record_id" {
+  description = "Cloudflare record ID for Grafana"
+  value       = var.create_grafana_record ? cloudflare_record.grafana[0].id : null
+}
+
+output "grafana_url" {
+  description = "Full URL for Grafana access"
+  value       = var.create_grafana_record ? "https://${var.grafana_subdomain}.${var.domain_name}" : null
+}
