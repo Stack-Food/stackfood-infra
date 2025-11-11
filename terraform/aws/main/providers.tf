f78b5983cbb99d6d4ca6febc06b1179ce8e2ea1b
LuizFelipeAlvesMaia <luiz.felipeam@hotmail.com>
2025-11-11 19:23:30 -0300

    fix: Update S3 bucket name for Terraform state management

--- terraform/aws/main/providers.tf
+++ terraform/aws/main/providers.tf
@@ -20,7 +20,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "stackfood-s3-tfstate-fiap"
+    bucket  = "stackfood-tfstate-soat04"
     key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
