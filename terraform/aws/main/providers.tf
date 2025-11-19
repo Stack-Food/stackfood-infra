bb415a52346ebd87ea3236475f2196e0e06e8ab3
LuizFelipeAlvesMaia <luiz.felipeam@hotmail.com>
2025-11-18 21:34:41 -0300

    feat: Update kubectl and kubernetes providers to use exec for EKS token retrieval

--- terraform/aws/main/providers.tf
+++ terraform/aws/main/providers.tf
@@ -35,7 +35,11 @@ terraform {
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+  }
 }
 provider "aws" {
   region = var.aws_region
@@ -62,12 +66,11 @@ data "aws_eks_cluster_auth" "eks" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   command     = "aws"
-  #   args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id, "--region", var.aws_region]
-  # }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+  }
 }
 
 # Configure Helm provider with EKS cluster config
