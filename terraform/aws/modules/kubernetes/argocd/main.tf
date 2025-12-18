7f73f3b9202196f6b67759b5d624b6500dc3529a
LuizFelipeAlvesMaia <luiz.felipeam@hotmail.com>
2025-12-18 20:17:25 -0300

    feat: Remove ArgoCD application creation logic from Helm release module

--- terraform/aws/modules/kubernetes/argocd/main.tf
+++ terraform/aws/modules/kubernetes/argocd/main.tf
@@ -23,25 +23,6 @@ resource "helm_release" "argocd" {
   ]
 }
 
-# Busca todos os arquivos YAML na pasta applications/
-locals {
-  application_files = fileset("${path.module}/applications", "*.yaml")
-
-  # Filtra o template para não criar Application dele
-  applications = {
-    for file in local.application_files :
-    trimsuffix(file, ".yaml") => file
-    if file != "application-template.yaml"
-  }
-}
-
-# Cria uma Application do ArgoCD para cada arquivo YAML na pasta applications/
-resource "kubernetes_manifest" "argocd_applications" {
-  for_each = local.applications
-
-  manifest = yamldecode(file("${path.module}/applications/${each.value}"))
-
-  depends_on = [
-    helm_release.argocd
-  ]
-}
+# NOTA: As applications do ArgoCD devem ser criadas manualmente via UI ou CLI
+# após o cluster estar completamente provisionado. Os manifestos de exemplo
+# estão disponíveis em: terraform/aws/modules/kubernetes/argocd/applications/
